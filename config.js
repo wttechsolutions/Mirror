e0ab7702b810b1adab5930084c85ff8854397891
Matthew Badali <Matthew Badali>
2016-04-11 05:21:00 -0500

    no message

---
+++ config.js
@@ -0,0 +1,35 @@
+var config = {
+    language : "en", 
+    layout: "main",
+    greeting : ["Hi, sexy!", "Hey There!", "Looking Awesome!"], 
+    forcast : {
+        key : "a6s5dg39j78qj38sjs91je9djadfa1e", 
+        units : "auto" 
+    },
+    hue : {
+        ip : "192.168.1.99", 
+        uername : "as9234ho0dfhoq01f2as3yh4m0", 
+        group : "0" 
+    },
+    calendar: {
+      icals : ["https://calendar.google.com/calendar/ical/mbadali25%40gmail.com/private-0c7a8be001f04c423eb7f5a3f209a4f0/basic.ics"],
+      maxResults: 9, 
+      maxDays: 365 
+    },
+    giphy: {
+      key : "a6s5dg39j78qj38sjs91je9djadfa1e" 
+    },
+    traffic: {
+      key : "a6s5dg39j78qj38sjs91je9djadfa1e",
+      mode : "Driving",
+      origin : "4040 Avondale Ave #304, Dallas, Tx 75219",
+      destination : "13350 Dallas Pkwy, Dallas, TX 75240",
+      name : "work",
+      reload_interval : 5
+    },
+	nest: {
+		username : "mbadali25@gmail.com",
+		password : "badali25"
+		
+	}
+};
